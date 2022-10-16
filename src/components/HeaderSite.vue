9f4ebb8f3ef1f81f9e91aafd2ec896a19cbb80fc
Michael Briquet <michael.br@free.fr>
2022-10-16 16:23:23 +0200

    ✨ Add main view and header component with menu.

---
+++ src/components/HeaderSite.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="header">
+    <div>
+      <img
+        src="../assets/icon-left-font-monochrome-black.png"
+        alt="Logo Groupomania"
+        class="header-logo"
+      />
+    </div>
+    <nav></nav>
+    <div class="action">
+      <a href="#" class="profile" @click.prevent.stop="menuToggle">
+        <img src="https://via.placeholder.com/52" alt="Image utilisateur" />
+      </a>
+      <div class="menu" :class="{ active: isMenuOpen }">
+        <p>Prenom Nom</p>
+        <ul>
+          <li><i class="fas fa-user"></i><a href="#">Mon Profile</a></li>
+          <li>
+            <i class="fas fa-sign-out-alt"></i><a href="#">Déconnexion</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSite',
+  data: () => {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    menuToggle() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.header {
+  padding: 0.2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.header-logo {
+  padding: 0.2rem;
+  height: 65px;
+}
+
+.action {
+  width: 52px;
+  height: 52px;
+}
+
+.action .profile {
+  position: relative;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  cursor: pointer;
+  display: block;
+}
+.action .profile img {
+  position: absolute;
+  top: 0;
+  right: 1.5rem;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.action .menu {
+  position: absolute;
+  top: 120px;
+  right: 0.3rem;
+  padding: 10px 20px;
+  background: #fff;
+  width: 200px;
+  box-sizing: 0 5px 25px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
+  transition: 0.5s;
+  visibility: hidden;
+  opacity: 0;
+}
+.action .menu.active {
+  top: 80px;
+  visibility: visible;
+  opacity: 1;
+}
+.action .menu::before {
+  content: '';
+  position: absolute;
+  top: -5px;
+  right: 28px;
+  width: 20px;
+  height: 20px;
+  background: #fff;
+  transform: rotate(45deg);
+}
+.action .menu p {
+  width: 100%;
+  text-align: center;
+  font-size: 18px;
+  padding: 5px 0;
+  font-weight: 500;
+  color: #555;
+  line-height: 1.2em;
+}
+
+.action .menu ul li {
+  list-style: none;
+  padding: 12px 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: left;
+  cursor: pointer;
+}
+.action .menu ul li i {
+  max-width: 20px;
+  margin-right: 20px;
+  color: #555;
+  transition: 0.3s;
+}
+.action .menu ul li:hover i {
+  color: #ff5d94;
+}
+.action .menu ul li a {
+  display: inline-block;
+  text-decoration: none;
+  color: #555;
+  font-weight: 500;
+  transition: 0.3s;
+}
+.action .menu ul li:hover a {
+  color: #ff5d94;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>Accueil</div>
+  <div class="container">
+    <HeaderSite />
+  </div>
 </template>
 <script>
+import HeaderSite from '@/components/HeaderSite.vue';
+
 export default {
   name: 'HomePage',
+  components: { HeaderSite },
 };
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+.container {
+  background-image: linear-gradient(
+    to right top,
+    #fd2d01,
+    #ff553b,
+    #ff7664,
+    #ff9489,
+    #ffb1ac,
+    #ffbebb,
+    #ffcac9,
+    #ffd7d7,
+    #ffd7d7,
+    #ffd7d7,
+    #ffd7d7,
+    #ffd7d7
+  );
+  width: 100%;
+  height: 100vh;
+}
+</style>
